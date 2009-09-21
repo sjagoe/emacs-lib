7b63ce6e593553c13d6b374571697b67814f5196
Simon Jagoe <azriphale@gmail.com>
2009-09-21 14:21:15 +0100

    Modified for emacs 23

--- dotemacs-custom.el
+++ dotemacs-custom.el
@@ -3,11 +3,12 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(color-theme-selection "Deep Blue" nil (color-theme)) ;; linux only
  '(blink-cursor-mode nil)
- '(indent-tabs-mode nil)
+ '(color-theme-selection "Deep Blue" nil (color-theme))
  '(column-number-mode t)
  '(ecb-options-version "2.32")
+ '(indent-tabs-mode nil)
+ '(org-agenda-files (quote ("~/Documents/org/jagoetech-portal.org" "~/Documents/org/portal_spike.org" "~/Documents/org/fidelis-infr.org" "~/Documents/org/jagoetech-comms.org" "~/Documents/org/business-administration.org" "~/Documents/org/maintenance.org")))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
@@ -18,5 +19,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :family "xos4-terminus"))))
+ '(default ((t (:stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(show-paren-match ((t (:background "DarkGreen" :foreground "yellow" :weight bold)))))
--- dotemacs.el
+++ dotemacs.el
@@ -15,11 +15,19 @@
 (yas/initialize)
 (yas/load-directory "~/.emacs-lib/lisp/yasnippet/snippets")
 
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
 ;; My modules
 (require 'sj-misc-custom)
 (require 'sj-paren)
 (require 'sj-cpp)
 (require 'sj-keys)
 ;;(require 'sj-ecb)
+
+(setq line-move-visual nil)
+(setq truncate-partial-width-windows t)
+(setq indent-tabs-mode nil)
+
+(desktop-save-mode t)
 
 ;;(require 'sj-windows)

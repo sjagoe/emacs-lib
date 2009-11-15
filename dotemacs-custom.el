1a8c6116949572425a6c63b190e5ebd3794ee21f
Simon Jagoe <simon.jagoe@pragmagility.com>
2009-11-15 09:58:46 +0000

    rst-mode background

--- dotemacs-custom.el
+++ dotemacs-custom.el
@@ -8,7 +8,7 @@
  '(column-number-mode t)
  '(ecb-options-version "2.32")
  '(indent-tabs-mode nil)
- '(org-agenda-files (quote ("~/Documents/org/jagoetech-portal.org" "~/Documents/org/portal_spike.org" "~/Documents/org/fidelis-infr.org" "~/Documents/org/jagoetech-comms.org" "~/Documents/org/business-administration.org" "~/Documents/org/maintenance.org")))
+ '(org-agenda-files (quote ("~/Documents/org/personal/something.org" "~/Documents/org/jagoetech-portal.org" "~/Documents/org/portal_spike.org" "~/Documents/org/fidelis-infr.org" "~/Documents/org/jagoetech-comms.org" "~/Documents/org/business-administration.org")))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
@@ -20,4 +20,9 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(rst-level-1-face ((t (:background "---"))) t)
+ '(rst-level-2-face ((t (:background "---"))) t)
+ '(rst-level-4-face ((t (:background "---"))) t)
+ '(rst-level-5-face ((t (:background "---"))) t)
+ '(rst-level-6-face ((t (:background "---"))) t)
  '(show-paren-match ((t (:background "DarkGreen" :foreground "yellow" :weight bold)))))

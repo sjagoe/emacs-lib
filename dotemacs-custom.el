722e815eea1e0a753e73b169f48d8e41d5d6ef95
Simon Jagoe <simon.jagoe@pragmagility.com>
2009-11-15 09:53:04 +0000

    Upped font size for master

--- dotemacs-custom.el
+++ dotemacs-custom.el
@@ -19,5 +19,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 65 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(show-paren-match ((t (:background "DarkGreen" :foreground "yellow" :weight bold)))))

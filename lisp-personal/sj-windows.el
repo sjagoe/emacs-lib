1670c7a4ba65949227137599408dc2e786b6308d
Simon Jagoe <sjagoe@enthought.com>
2012-02-01 16:39:06 -0500

    Updated Windows support

--- lisp-personal/sj-windows.el
+++ lisp-personal/sj-windows.el
@@ -1,8 +1,8 @@
 
-;; Colour Theme
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-deep-blue)
+;; ;; Colour Theme
+;; (require 'color-theme)
+;; (color-theme-initialize)
+;; (color-theme-deep-blue)
 
 ;; (add-to-list 'default-frame-alist '(font . "-outline-Courier New-normal-r-normal-normal-11-82-96-96-c-*-iso8859-1"))
 ;; (add-to-list 'default-frame-alist '(font . "-*-Monaco-normal-r-*-*-17-102-120-120-c-*-iso8859-1"))
@@ -25,11 +25,17 @@
 	  (equal (string-match "^[A-Za-z]:[/\\]" default-directory) 0)))
 
 (require 'powershell-mode)
+(setq auto-mode-alist
+      (append '(("\\.ps1$" . powershell-mode)) auto-mode-alist))
+
 
 (require 'tramp)
 (setq tramp-auto-save-directory "c:\\dev\\tmp")
 (setq tramp-default-method "plink")
 
 (setq ediff-split-window-function 'split-window-horizontally)
+
+;; vc-git is really slow in windows ...
+(setq vc-handled-backends nil)
 
 (provide 'sj-windows)

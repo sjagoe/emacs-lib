7ec88dda40542ff469549b6fdf62fc4001a5ec11
Simon Jagoe <sjagoe@enthought.com>
2013-02-07 16:53:58 +0000

    Reduced font size

--- dotemacs-custom.el
+++ dotemacs-custom.el
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 113 :family "DejaVu Sans Mono"))))
+ '(default ((t (:height 92 :family "DejaVu Sans Mono"))))
  '(flycheck-error-face ((t (:background "gray50" :slant oblique))))
  '(flycheck-warning-face ((t (:background "gray35" :slant oblique))))
  '(magit-item-highlight ((t nil)))
